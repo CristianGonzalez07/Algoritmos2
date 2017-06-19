545376993a2babebbd5b64fa30ccb56656129a46
Cristian <1995.cgonzalez.07@gmail.com>
2017-06-19 18:55:47 -0300

    ej 1 tp1 commented

--- tp1/Ej1/ejercicio1.hs
+++ tp1/Ej1/ejercicio1.hs
@@ -2,92 +2,144 @@ import Data.List
 {-
     Cada Delta esta formado por un conjunto de transiciones,
     cada transicion tiene el siguiente formato:
-    ((estado del cual se parte, caracter involucrado), estado final)
+    ((estado inicial,caracter involucrado), estado final)
 -}
 type Delta = [((Int,Char),Int)] 
 
-type DFA = ([Int],[Char],Delta,Int,[Int])  --(conjunto de estados, sigma, delta, estado inicial, estado final)
+{-
+	El tipo DFA tiene el siguiente formato:
+	(conjunto de estados, sigma, delta, estado inicial, estado final)
+-}
+type DFA = ([Int],[Char],Delta,Int,[Int])  
 
---O(n!)
---Retorna todos los subconjuntos
+{-
+	Tiempo de ejecucion en el peor caso O(n!)
+	donde n! = tamaño de la lista dada.
+
+	Funcion que reconoceCadena una lista y retorna 
+	todos los subconjuntos posibles de la misma
+-}
 subconjuntos :: [a]->[[a]]
 subconjuntos [] = [[]]
 subconjuntos (x:xs)= sub++[x:ys|ys<- sub]
                                 where sub =subconjuntos xs
 
 {-
-    Genera todos los posibles deltas con el formato de el tipo definido anteriormente
-    para una cantidad maxima de estados y un sigma que toma como parametro.
--}
---O(n!)
-generateDelta :: [Int] ->[Char] -> Delta
-generateDelta xs ys = [((r,t),p)|r <- xs, t<-ys, p<-xs]
+	Tiempo de ejecucion en el peor caso O(n!) donde
+	n = cantidad de estados + cantidad de caracteres del alfabeto.
 
+  Funcion que dado un sigma como parametro retorna todos los
+  posibles deltas  con el formato de el tipo definido 
+  anteriormente para una cantidad maxima de estados
+-} 
+
+generarDelta :: [Int] ->[Char] -> Delta
+generarDelta xs ys = [((qi,c),qf)|qi <- xs, c<-ys, qf<-xs]
+
+{-
+   	Tiempo de ejecucion en el peor caso O(n)donde 
+   	n = longitud de la lista dada como delta.  
+
+    Funcion que dado un caracter, un estado inicial y un delta
+    retorna una tupla con el formato (bool,int) donde el bool
+    sera true si el caracter es reconocido por ese delta
+    (caso contrario false) y el int sera el estado final donde
+    culmine esa evaluacion.
+-}
+
+reconoceCaracter :: Char -> Int -> Delta -> (Bool,Int) 
+reconoceCaracter x n [] = (False,n)
+reconoceCaracter x n d
+                    	| (n,x) == fst d1 = (True,(snd d1))  
+                        | otherwise     = ( (False || fst (reconoceCaracter x n (tail d))),snd (reconoceCaracter x n (tail d))) 
+                       		where d1 = head d
 
 
 {-
-    Hace la prueba sobre un delta  para cada caracter del string a reconocer(o no).
-    Dado un caracter, un estado inicial y un delta retorna true si el caracter es reconocido por ese delta
-    y tambien retorna el estado final en el cual queda luego de la evaluacion.
--}
---O(n)
-pruebaDelta :: Char -> Int -> Delta -> (Bool,Int) 
-pruebaDelta x n [] = (False,n)
-pruebaDelta x n d
-                             | (n,x) == fst d1 = (True,(snd d1))  
-                             | otherwise     = ( (False || fst (pruebaDelta x n (tail d))),snd (pruebaDelta x n (tail d))) 
-                         where d1 = head d
+	Tiempo de ejecucion en el peor caso O(n*m) donde
+	n = longitud de la cadena ,m es el tiempo en el peor
+	caso de la funcion reconoceCaracter.
 
-
-{-
-    Funcion que en base a una cadena, un sigma, un estado inicial, un estado final y un delta,
-    determina si la cadena es reconocido por el delta. 
+    Funcion que dados a una cadena, un alfabeto,un estado inicial,
+    un estado final y un delta,retorna True si la cadena dada
+    es reconocida por el delta dado.Caso contrario retorna
+    false.
 -}
---O(n2)
-recibe :: String -> [Char] -> Int ->Int -> Delta -> Bool 
-recibe [] e ei ef d = (ei == ef)
-recibe (x:xs) e ei ef d 
-      | elem x e = ((fst st)) && (recibe xs e (snd st) ef d)  
-      | otherwise        = False
-      where st = pruebaDelta x ei d
+
+reconoceCadena :: String -> [Char] -> Int ->Int -> Delta -> Bool 
+reconoceCadena [] e ei ef d = (ei == ef)
+reconoceCadena (x:xs) e ei ef d 
+    							| elem x e = ((fst st)) && (reconoceCadena xs e (snd st) ef d)  
+     							| otherwise        = False
+      								where st = reconoceCaracter x ei d
 
 {-
-    Funcion que evalua si una cadena cumple con un delta en particular
-    Los parametros que recibe son: 
-    conjunto de cadenas -> sigma -> estado inicial -> estado final -> delta
--}
---O(n3)
-----devolver lista y emezar desde aca
+	Tiempo de ejecucion en el peor caso O(n*m) donde
+	n = cantidad de cadenas positivas m es el tiempo 
+  en el peor caso de reconoceCadena.
+
+  Funcion que dada una lista de cadenas(positivas),
+  un conjunto de estados,un estado inicial,un estado final
+  y un delta, retorna true si la cadena dada es reconocida
+  por el delta dado.Caso contrario retorna false.
+-} 
+
 evaluaCadenaPos :: [String] -> [Char] -> Int ->Int -> Delta ->Bool
 evaluaCadenaPos [] _ _ _ _ = True
-evaluaCadenaPos (x:xs) sigma ei ef delta = (recibe x sigma ei ef delta) && (evaluaCadenaPos xs sigma ei ef delta)
+evaluaCadenaPos (x:xs) sigma ei ef delta = (reconoceCadena x sigma ei ef delta) && (evaluaCadenaPos xs sigma ei ef delta)
 
---retorna el conj de estados finales
---estados->sigma->delta ->strings pos
---O(n)
+{-
+	Tiempo de ejecucion en el peor caso O(n) donde
+	n = longitud de lista de cadenas positivas.
+
+	Funcion que dados un conjunto de estados,un alfabeto,un delta
+	y un conjunto de cadenas(positivas) retorna un subconjunto
+	de estados correspondiente a los estados finales del automata
+-}
+
 finales :: [Int] -> [Char]->Delta ->[String]-> [Int]
 finales _ _ _ [] = []
-finales st sigma delta (x:xs) = [s2 | s1<-st, s2<-st, recibe x sigma s1 s2 delta ] ++ (finales st sigma delta xs)
+finales st sigma delta (x:xs) = [s2 | s1<-st, s2<-st, reconoceCadena x sigma s1 s2 delta ] ++ (finales st sigma delta xs)
 
---O(n3)
+{-
+	Tiempo de ejecucion en el peor caso O(n*m) donde
+	n = cantidad de cadenas negativas m es el tiempo en el peor
+  caso de reconoceCadena.
+
+  Funcion que dada una lista de cadenas(negativas),
+  un conjunto de estados,un estado inicial,un estado final
+  y un delta, retorna true si la cadena dada es reconocida
+  por el delta dado.
+-}
+
 evaluaCadenaNeg :: [String] -> [Char] -> Int ->Int -> Delta -> Bool 
 evaluaCadenaNeg [] _ _ _ _ = True
 evaluaCadenaNeg (x:xs) sigma ei ef delta 
-                                                                    | (recibe x sigma ei ef delta== False)= (evaluaCadenaNeg xs sigma ei ef delta)
-                                                                    | otherwise = False
+                                   	    | (reconoceCadena x sigma ei ef delta== False)= (evaluaCadenaNeg xs sigma ei ef delta)
+                                        | otherwise = False
+{-
+	Tiempo de ejecucion en el peor caso O(n) donde
+	n = tamaño del delta.
 
+	Funcion que dada una transicion y un delta,retorna true si
+	la transicion es determinista dentro de ese delta. 
+-}
 
-
---O(n)
--- Funcion que evalua si una de las transiciones de un delta es valida para ser determinista.
 validaDelta :: ((Int,Char),Int) -> Delta -> Bool
 validaDelta (x1,y1) []     = True
 validaDelta (x1,y1) (y:ys) 
                                 | x1 == (fst y) = False
                                 | otherwise =  (validaDelta (x1,y1) ys) 
 
---O(n2)
--- Funcion que dado un delta, determina si todas las transisciones del mismo lo hacen determinista o no.
+{-	
+	Tiempo de ejecucion en el peor caso O(n*m) donde 
+	n = tamaño del delta m es el tiempo en el peor caso
+  de validaDelta.
+	
+	Funcion que dado un delta retorna true si todas las transiciones
+	del mismo lo hacen determinista.Caso contrario retorna false.
+-}
+
 filtraDeltas :: Delta -> Bool
 filtraDeltas [] = True
 filtraDeltas (x:xs) 
@@ -95,11 +147,19 @@ filtraDeltas (x:xs)
                 | otherwise  = (validaDelta x xs) && filtraDeltas xs
 
 {-
-    Funcion que busca el primer delta que verifique que se cumplan todas las cadenas positivas y no las negativas
-    La funcion toma como parametros:
-     sigma -> cadena de los "pos" -> cadena de los "neg" -> cantidad maxima de estados -> conjunto de todos los delta
+	Tiempo de ejecucion en el peor caso O(n*m*p) donde
+	n = tamaño del delta,m = tiempo en el peor caso de evaluaCadenaPos,
+  p = tiempo en el peor caso de evaluaCadenaNeg
+	
+	Funcion que dados un conjunto de estados,un conjunto de cadenas
+	positivas,un conjunto de cadenas negativas,una cantidad maxima de
+	estados y una lista de deltas, retorna una tupla donde la primer
+	componente es el primer delta que verifique reconocer todas las
+	cadenas positivas y no reconocer ninguna cadena negativa.
+	Y como segunda componente el conjunto con los estados finales
+	de dicho delta.
 -}
---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 encuentraDelta :: [Char] -> [String] -> [String] -> Int -> [Delta] -> (Delta,[Int] )
 encuentraDelta _ _ _ _ [] = ([],[])
 encuentraDelta sigma pos neg k (x:xs)
@@ -108,10 +168,18 @@ encuentraDelta sigma pos neg k (x:xs)
                                                 where f = finales [0..(k-1)] sigma x pos
 
 {- 
-  Funcion que busca el minimo delta que cumpla con las restricciones de ser valido para los pos y no para los neg para 
-  el menor numero de estados posibles empezando de un unico estado (representado como el estado "0"), si no 
-  encuentra ninguno, retorna el delta vacio.
-  Retorna el delta encontrado y el estado final que posee el automata.
+  Tiempo de ejecucion en el peor caso O(n*k)
+  donde k es la cantidad de estados maximos  y n es el tiempo
+  en el peor caso de encuentraDelta.
+
+  Funcion que dados un conjunto de estados,un conjunto de cadenas
+  positivas,un conjunto de cadenas negativas,una cantidad maxima de
+  estados,un acumulador inicialmente como 0 y una lista de deltas,
+  retorna una tupla donde la primer componente es (en caso de existir)
+  el minimo delta que cumpla con las restricciones de ser valido para
+  las cadenas positivas y no valido para ninguna de las cadenas
+  negativas para el menor numero de estados posibles empezando de un 
+  unico estado (representado como el estado "0").y
 -}
 minimoDelta :: [Char] -> [String] -> [String] -> Int -> Int -> [Delta] -> (Delta,[Int]) 
 minimoDelta sigma pos neg n k b
@@ -120,15 +188,25 @@ minimoDelta sigma pos neg n k b
                                             where a =(encuentraDelta sigma pos neg n b)
 
 {-
-    Funcion principal del programa con el cual se obtiene un automata (DFA) 
-    La funcion recibe los parametros de la siguiente forma: 
-      sigma -> lista de estados "pos", lista de estados "neg"-> cantidad maxima de estados k
+  Tiempo de ejecucion en el peor caso O(n+m+p).
+  donde n es el tiempo en el peor caso de generarDelta,
+  m es el tiempo en el peor caso de
+  filter (filtraDeltas) ((subconjuntos a)) = 
+  y p es el tiempo en el peor caso de minimoDelta.
+
+  Funcion principal del programa,dado un alfabeto,un conjunto de 
+  cadenas positivas,un conjunto de cadenas negativas y un k maximo 
+  para limitar el numero de estados, retorna un DFA (en caso de que exista)
+  que cumpla con las restricciones de ser valido para las cadenas positivas
+  y no valido para ninguna de las cadenas negativas para el menor numero de
+  estados posibles 
 -}
-check :: [Char] -> [String] -> [String] -> Int -> DFA 
-check sigma pos neg k = 
+
+main :: [Char] -> [String] -> [String] -> Int -> DFA 
+main sigma pos neg k = 
                                         let 
-                                          a = generateDelta [0..(k-1)] sigma
+                                          a = generarDelta [0..(k-1)] sigma
                                           b = filter (filtraDeltas) ((subconjuntos a)) -- Se aplica el reverse con el fin de empezar 
-                                          c = (minimoDelta sigma pos neg 0 k b)                      -- las evaluaciones con la minima cantidad 
-                                        in                                                                                   --  de estados posibles    
+                                          c = (minimoDelta sigma pos neg 0 k b)        -- las evaluaciones con la minima cantidad 
+                                        in                                             -- de estados posibles    
                                           ([0..(k-1)],sigma,(fst c),0, nub(snd c))
